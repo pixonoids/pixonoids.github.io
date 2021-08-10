cdda198d1af647ddb99afd2fbadb4c59292b0c74
Rohit Kaushal <rohitkk252550@gmail.com>
2021-08-10 12:28:13 +0530

    🎉 init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pixonoids</h1>
+      <p>
+        The Official Photography , Animation , Graphic Designing , Video Editing
+        and Web Development Club of National Institute Of Technology Hamirpur
+      </p>
+    </div>
+  );
+}
+
+export default App;
